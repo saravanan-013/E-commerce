9eb5f04fcd9d95cc1d75160c51418bdb66910401
saravanan-013 <saravanan13093@gmail.com>
2024-05-24 08:25:07 +0530

    change

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
     const [searchParams, setSearchParams] =  useSearchParams()
 
     useEffect(() => {
-        fetch(process.env.REACT_APP_API_URL+'/products?'+searchParams)
+        fetch(process.env.RENDER_APP_API_URL+'/products?'+searchParams)
         .then(res => res.json())
         .then( res => setProducts(res.products))
     },[searchParams])
