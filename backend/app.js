4e5f82e9206aa7f3d5fb1e4e460319670a591449
saravanan-013 <saravanan13093@gmail.com>
2024-05-24 09:30:12 +0530

    change

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const orders = require('./routes/order');
 connectDatabase();
 
 app.use(express.json());
-app.use(cors({origin:"http://localhost:3000"}));
+app.use(cors({origin:"https://e-commerce-eta-fawn.vercel.app"}));
 
 
 
