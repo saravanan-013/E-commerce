a57e5246317492daafc53e79ccb1dd6ce1d55ace
saravanan-013 <saravanan13093@gmail.com>
2024-05-24 10:09:02 +0530

    change

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -26,6 +26,27 @@ export default function Home() {
         fetchProducts();
     }, [searchParams]);
 
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch(`${URL}/products?${searchParams}`);
+                
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+
+                const data = await response.json();
+                setProducts(data.products);
+            } catch (err) {
+                console.error('Error fetching products:', err.message);
+                setProducts([]);
+               
+            }
+        };
+
+        fetchProducts();
+    }, [searchParams]);
+
 
     return <Fragment>
         <h1 id="products_heading">Latest Products</h1>
